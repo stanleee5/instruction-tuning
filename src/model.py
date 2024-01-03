fd172ca3613347fdc49bff3f5f9dc8bf202927ac
stanleee5 <stanleee5@gmail.com>
2024-01-03 09:14:54 +0000

    add support: phi-2 model

--- src/model.py
+++ src/model.py
@@ -6,7 +6,12 @@ from typing import Optional
 
 import torch
 from loguru import logger
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoConfig, AutoModelForCausalLM, AutoTokenizer
+
+from src.utils import is_main_process
+
+if not is_main_process():
+    logger.remove()
 
 
 @dataclass
@@ -29,11 +34,25 @@ def load_model(
     gradient_checkpointing: bool,
 ) -> torch.nn.Module:
     logger.info("Loading model")
+    config = AutoConfig.from_pretrained(model_name_or_path, trust_remote_code=True)
+
+    if config.model_type == "llama":
+        flash_params = {"use_flash_attention_2": use_flash_attn}
+    elif config.model_type == "phi-msft":
+        flash_params = {
+            "flash_attn": use_flash_attn,
+            "flash_rotary": use_flash_attn,
+            "fused_dense": use_flash_attn,
+        }
+        # NOTE: gradient_checkpointing should be False (tarining_args)
+    else:
+        flash_params = {}
+
     model = AutoModelForCausalLM.from_pretrained(
         model_name_or_path,
-        use_flash_attention_2=use_flash_attn,
         load_in_8bit=load_in_8bit,
         trust_remote_code=True,
+        **flash_params,
     )
 
     # enable gradient checkpointing
@@ -49,7 +68,10 @@ def load_model(
 def load_tokenizer(model_name_or_path: str):
     logger.info("Loading tokenizer")
     tokenizer = AutoTokenizer.from_pretrained(
-        model_name_or_path, padding_side="right", add_prefix_space=False
+        model_name_or_path,
+        padding_side="right",
+        add_prefix_space=False,
+        trust_remote_code=True,
     )
     if tokenizer.pad_token is None:
         tokenizer.add_special_tokens({"pad_token": tokenizer.eos_token})
