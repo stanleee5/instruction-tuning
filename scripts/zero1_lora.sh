a68235b5e374a0ccef79f365ced000b838a62c4a
stanleee5 <stanleee5@gmail.com>
2024-03-12 07:19:10 +0000

    move deepspeed configs to general config root

--- scripts/zero1_lora.sh
+++ scripts/zero1_lora.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=0 accelerate launch \
     --num_machines 1 \
     --num_processes 1 \
     --use_deepspeed \
-    --deepspeed_config_file ./ds_configs/zero1.json \
+    --deepspeed_config_file ./configs/deepspeed/zero1.json \
     train.py \
     --bf16 True \
     --model_name_or_path JackFram/llama-160m \
